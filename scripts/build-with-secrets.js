d2504eb57c6322079098d4dfb7db24faca5d93ce
Prerith Subramanya <prerithsubramanya@gmail.com>
2025-07-19 16:56:50 +1000

    updated build_with_script

--- scripts/build-with-secrets.js
+++ scripts/build-with-secrets.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Read environment variables
 const OAUTH2_CLIENT_ID = process.env.OAUTH2_CLIENT_ID || '{{OAUTH2_CLIENT_ID}}';
